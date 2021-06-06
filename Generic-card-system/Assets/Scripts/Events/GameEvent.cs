81383c590c4f169a68f761ba74ce445cd230f74f
Marco <gd60marco@vfs.com>
2021-06-05 20:39:37 -0700

    Game Events

---
+++ Generic-card-system/Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Marco, Marco 26th 2021
+public abstract class GameEvent<T> : ScriptableObject
+{
+    //Class Fields ___________________________________________
+    public UnityEvent<T> OnInvoke;
+
+
+    //Custom Methods ________________________________________
+    public void Invoke(T param)
+    {
+        OnInvoke.Invoke(param);
+    }
+}
---
+++ Generic-card-system/Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Marco, March 26th 2021
+public abstract class GameEventListener<T> : MonoBehaviour
+{
+    //Class Fields _______________________________________________
+    [SerializeField] private GameEvent<T> _gameEvent;
+    public UnityEvent<T> OnInvoke;
+    
+    //Unity Functions ____________________________________________
+    private void OnEnable()
+    {
+        _gameEvent.OnInvoke.AddListener(GameEventInvoked);
+    }
+
+    private void OnDisable()
+    {
+        _gameEvent.OnInvoke.RemoveListener(GameEventInvoked);
+    }
+
+    //Custom Methods ___________________________________________
+    private void GameEventInvoked(T param)
+    {
+        OnInvoke.Invoke(param);
+    }
+}
